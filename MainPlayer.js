163505ef81bf28d945583be296c08575f7c69bf6
compromise-satisfaction <44921951+compromise-satisfaction@users.noreply.github.com>
2019-09-30 02:07:45 +0900

    a

---
+++ MainPlayer.js
@@ -0,0 +1,18 @@
+class MainPlayer{
+  //コンストラクタでは
+  constructor(){
+
+    //ここのステースを追加して条件フラグなどを複雑化できます
+    this.status = {
+      'ainepoint': 0,
+      '解説を希望': false,
+      '解説を拒否': false,
+      'ラブミーティア結成を復習': false,
+      '盗聴を決意': false,
+      '包丁を装備': false,
+    };
+
+  }
+
+
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# enchantjs_novel_engine
+enchantjsでノベルゲーム（ギャルゲー）を作るエンジンっぽいもの
+
+enchant.js
+tl.enchant.js
+
+は削除してあります。
+
+デモ
+http://poridiet.com/sinyukanojo/sinyukanojo/index.html
