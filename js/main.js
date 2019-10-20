0b22441e20072d18653897b226801e511ec4c06a
compromise-satisfaction <44921951+compromise-satisfaction@users.noreply.github.com>
2019-10-21 03:07:49 +0900

    a

--- js/main.js
+++ js/main.js
@@ -36,49 +36,69 @@ function Load(width,height){
       scene.addChild(Continuation);
 
       Beginning.addEventListener('touchstart',function(e){
-        core.replaceScene(MainScene(1));
+        core.replaceScene(MainScene(1,1,1,1,1));
       });
 
       Continuation.addEventListener('touchstart',function(e){
-        core.replaceScene(MainScene(1));
+        core.replaceScene(MainScene(1,1,1,1,1));
       });
 
       return scene;
     };
-    var MainScene = function(Number){
+    var MainScene = function(B_Number,C1_Number,C1_frame,C2_Number,C2_frame){
       var scene = new Scene();                                // 新しいシーンを作る
 
       var Background = new Sprite(1600,900);
-      Background.image = core.assets["image/背景/"+ Number +".png"];
+      Background.image = core.assets["image/背景/"+ B_Number +".png"];
       Background.x = 0;
       Background.y = 0;
       scene.addChild(Background);
 
       var Character1 = new Sprite(800,900);
-      Character1.image = core.assets["image/キャラ/"+ Number +".png"];
+      Character1.image = core.assets["image/キャラ/"+ C1_Number +".png"];
       Character1.x = 0;
       Character1.y = 0;
+      Character1.frame = C1_frame;
       Character1.opacity = 0;
       Character1.tl.fadeIn(20);
       scene.addChild(Character1);
 
+      var Character2 = new Sprite(800,900);
+      Character2.image = core.assets["image/キャラ/"+ C2_Number +".png"];
+      Character2.x = 800;
+      Character2.y = 0;
+      Character2.frame = C2_frame;
+      Character2.opacity = 0;
+      Character2.tl.fadeIn(20);
+      scene.addChild(Character2);
+
+      var Text = new Label();
+      Text.font  = "60px monospace";
+      Text.color = 'black';
+      Text.x = 0;
+      Text.y = 900;
+      Text.width = 1600;
+      Text.height = 1600;
+      Text.text = ("【未知の決闘者】\n");
+      scene.addChild(Text);
+
       var Enter = new Label();
-      Enter.font  = "200px monospace";
+      Enter.font  = "60px monospace";
       Enter.color = 'black';
-      Enter.x = 0;
-      Enter.y = 900;
-      Enter.width = 1600;
-      Enter.height = 200;
+      Enter.x = width-180;
+      Enter.y = height-65;
+      Enter.width = 180;
+      Enter.height = 60;
       Enter.text = ("進む ▶");
       scene.addChild(Enter);
 
       var Return = new Label();
-      Return.font  = "200px monospace";
+      Return.font  = "60px monospace";
       Return.color = 'black';
       Return.x = 0;
-      Return.y = 1100;
-      Return.width = 1600;
-      Return.height = 200;
+      Return.y = height-65;
+      Return.width = 180;
+      Return.height = 60;
       Return.text = ("◀ 戻る");
       scene.addChild(Return);
 
