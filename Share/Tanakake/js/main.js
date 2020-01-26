16f82e42eb78065662834fa02a3b668cfae53128
compromise-satisfaction <44921951+compromise-satisfaction@users.noreply.github.com>
2020-01-27 02:40:29 +0900

    a

--- Share/Tanakake/js/main.js
+++ Share/Tanakake/js/main.js
@@ -1059,7 +1059,7 @@ function Load(width,height){
       if(Version_new){
         Setting_Flag = window.localStorage.getItem("Setting_Flag").split(",");
         if(Setting_Flag[1]=="不動"&&Setting_Flag[0]=="遊星"&&Setting_Flag[2]=="男") Submit("テスト用");
-        if(Setting_Flag[1]=="妥協"&&Setting_Flag[0]=="満足"&&Setting_Flag[2]=="未設定") Submit("テスト用");
+        else if(Setting_Flag[1]=="妥協"&&Setting_Flag[0]=="満足"&&Setting_Flag[2]=="未設定") Submit("テスト用");
         else if(Already);
         else {
           fetch(GAS,)
