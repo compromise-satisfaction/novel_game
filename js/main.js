5a1e909c9ccca50172a39431021d5049ac5ed605
compromise-satisfaction <44921951+compromise-satisfaction@users.noreply.github.com>
2020-01-30 10:15:19 +0900

    a

--- js/main.js
+++ js/main.js
@@ -50,10 +50,8 @@ function Datas_load(width,height){
       if(Image_DATAS[i].url.substring(0,4)!="http"){
         Image_DATAS[i].url = "https://raw.githubusercontent.com/compromise-satisfaction/Saved/master/画像/" + Image_DATAS[i].url;
       }
-      else if(Image_DATAS[i].url.length>18){
-        if(Image_DATAS[i].url.substring(0,18)=="https://gyazo.com/"){
-          Image_DATAS[i].url = "https://i."+Image_DATAS[i].substring(8)+".png\")";
-        }
+      else if(Image_DATAS[i].url.substring(0,18)=="https://gyazo.com/"){
+          Image_DATAS[i].url = "https://i."+Image_DATAS[i].url.substring(8)+".png\")";
       }
       Image_urls[i] = Image_DATAS[i].url;
     }
