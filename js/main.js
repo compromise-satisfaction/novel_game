93fd207f856312cd359863a5ca588a061d0c86b0
compromise-satisfaction <44921951+compromise-satisfaction@users.noreply.github.com>
2020-02-01 15:25:24 +0900

    a

--- js/main.js
+++ js/main.js
@@ -3200,7 +3200,7 @@ function Game_load(width,height){
         Text[i] = new Sprite();
         Text[i]._element = document.createElement("innerHTML");
         Text[i]._style.font  = width/20+"px monospace";
-        Text[i].width = width-width/4;
+        Text[i].width = width-width/8-width/20;
         Text[i].x = width/8;
         Text[i].y = (width/4) + ((width/20)+(width/25)*(18+i*2)) - (width/25);
         scene.addChild(Text[i]);
