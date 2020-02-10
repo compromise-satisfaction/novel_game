a3bd4ce0b6064c1efd2cde970d884002c9c2c3dc
compromise-satisfaction <44921951+compromise-satisfaction@users.noreply.github.com>
2020-02-10 12:57:40 +0900

    a

--- js/main.js
+++ js/main.js
@@ -52,7 +52,48 @@ function Game_load(width,height,private){
       }
       else if(Type=="シーンデータ読み込み"){
         if(Play_Sheet==ID){
-          if(test) Scene_loads("テスト用",false,false,false,false,false);
+          if(test){
+              for (var i = 0; i < Main_DATAS.length; i++) {
+                Log_Flag[i] = Play_Sheet+Main_DATAS[i].シーン名 + "プレイ済み";
+              }
+              for (var k = 0; k < Choice_DATAS.length; k++) {
+                Log_Flag[i+k] = Play_Sheet+Choice_DATAS[k].シーン名 + "プレイ済み";
+              }
+              for (var i = 0; i < I_C_F_T_DATAS.length; i++) {
+                switch (I_C_F_T_DATAS[i].タイプ) {
+                  case "アイテム":
+                    if(I_C_F_T_DATAS[i].アイテムor人物orフラグ名orトロフィー名.indexOf("消滅")==-1){
+                      Item_Flag[Item_Flag.length] = [
+                        I_C_F_T_DATAS[i].アイテムor人物orフラグ名orトロフィー名.split("→")[I_C_F_T_DATAS[i].アイテムor人物orフラグ名orトロフィー名.split("→").length-1],
+                        I_C_F_T_DATAS[i].説明文,
+                        I_C_F_T_DATAS[i].画像,
+                        I_C_F_T_DATAS[i].詳細文,
+                        I_C_F_T_DATAS[i].詳細内容,
+                        I_C_F_T_DATAS[i].コード
+                      ];
+                    }
+                    break;
+                  case "人物":
+                    if(I_C_F_T_DATAS[i].アイテムor人物orフラグ名orトロフィー名.indexOf("消滅")==-1){
+                      Character_Flag[Character_Flag.length] = [
+                        I_C_F_T_DATAS[i].アイテムor人物orフラグ名orトロフィー名.split("→")[I_C_F_T_DATAS[i].アイテムor人物orフラグ名orトロフィー名.split("→").length-1],
+                        I_C_F_T_DATAS[i].説明文,
+                        I_C_F_T_DATAS[i].画像,
+                        I_C_F_T_DATAS[i].詳細文,
+                        I_C_F_T_DATAS[i].詳細内容,
+                        I_C_F_T_DATAS[i].コード
+                      ];
+                    }
+                    break;
+                  case "フラグ":
+                    Flag[Flag.length] = I_C_F_T_DATAS[i].アイテムor人物orフラグ名orトロフィー名;
+                    break;
+                  default:
+                    break;
+                }
+              }
+              Scene_loads("テスト用",false,false,false,false,false);
+            }
           else Scene_loads("最初",false,false,false,false,false);
           return;
         }
