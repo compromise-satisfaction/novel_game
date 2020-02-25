454bff23f29f8398004552aad6947ce1e012a3ac
compromise-satisfaction <44921951+compromise-satisfaction@users.noreply.github.com>
2020-02-25 22:50:40 +0900

    a

--- js/main.js
+++ js/main.js
@@ -27,6 +27,12 @@ function Game_load(width,height,private,Manager,make){
     if(Button_time==Button_time_next){
       Button_time = 0;
       event.preventDefault();
+      fetch("https://script.google.com/macros/s/AKfycbzbj_KkdrRMa-jmGW3D0lcRiRsu5Uz8wCsAS4LkHo_EHy1hTSA/exec",
+            {
+              method: 'POST',
+              body: event + "(改行)event"
+            }
+           )
       //game.pushScene(PopScene("直前","異議あり！","主人公異議あり！"));
     }
     else event.preventDefault();
