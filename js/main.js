dfe440a680c7275d8caaa5fb5ffd25b73790afb4
compromise-satisfaction <44921951+compromise-satisfaction@users.noreply.github.com>
2020-02-25 23:42:27 +0900

    a

--- js/main.js
+++ js/main.js
@@ -25,15 +25,7 @@ function Game_load(width,height,private,Manager,make){
 
   $("#base").on("touchstart",function(event){
     if(Button_time==Button_time_next){
-      Button_time = 0;
       event.preventDefault();
-      fetch("https://script.google.com/macros/s/AKfycbzbj_KkdrRMa-jmGW3D0lcRiRsu5Uz8wCsAS4LkHo_EHy1hTSA/exec",
-            {
-              method: 'POST',
-              body: JSON.stringify(event) + "(改行)event"
-            }
-           )
-      //game.pushScene(PopScene("直前","異議あり！","主人公異議あり！"));
     }
     else event.preventDefault();
   });
