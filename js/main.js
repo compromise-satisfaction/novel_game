5a280de06f0379ac32860e088051dbf9bc3a36bd
compromise-satisfaction <44921951+compromise-satisfaction@users.noreply.github.com>
2020-02-25 20:05:23 +0900

    a

--- js/main.js
+++ js/main.js
@@ -24,12 +24,10 @@ function Game_load(width,height,private,Manager,make){
   });
 
   $("#base").on("touchstart",function(event){
-    //if(Button_time==Button_time_next){
-      //Button_time = 0;
-  //  }
-    //else event.preventDefault();
-    event.preventDefault();
-    console.log(event);
+    if(Button_time==Button_time_next){
+      Button_time = 0;
+    }
+    else event.preventDefault();
   });
 
   var Syougen_time = 0;
