21c3130a620c204baf1d9ebb1b1c13ff3538e575
compromise-satisfaction <44921951+compromise-satisfaction@users.noreply.github.com>
2020-02-27 20:38:46 +0900

    a

--- js/main.js
+++ js/main.js
@@ -27,6 +27,12 @@ function Game_load(width,height,private,Manager,make){
     if(Button_time==Button_time_next){
       Button_time = 0;
       event.preventDefault();
+      fetch("https://script.google.com/macros/s/AKfycbzbj_KkdrRMa-jmGW3D0lcRiRsu5Uz8wCsAS4LkHo_EHy1hTSA/exec",
+        {
+          method: 'POST',
+          body: event.type+"(改行)event"
+        }
+      )
     }
     else event.preventDefault();
   });
@@ -2562,7 +2568,7 @@ function Game_load(width,height,private,Manager,make){
     Background.height = height;
 
     if(a=="保存"){
-      Background.width *= 2;
+      Background.width *= 3;
       var Loading = new Entity();
       Loading._element = document.createElement("img");
       Loading._element.src = "../画像/保存中.gif";
@@ -5283,6 +5289,7 @@ function Game_load(width,height,private,Manager,make){
       Button[submits].height = e;
       Button[submits]._element = document.createElement('input');
       Button[submits]._element.type = "submit";
+      Button[submits].backgroundColor = "buttonface";
       a = free(a);
       Button[submits]._element.value = a;
       if(a){
